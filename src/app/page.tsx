aed62f5363c144ae8027fc24c67bf6c3c32198a8
Mat <matze.krieger@googlemail.com>
2025-11-21 11:44:36 +0100

    added picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,22 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+
+  console.log('executing...');
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <Image
+            src="/full-stack-logo.png"
+            alt="full stack logo"
+            width={200}
+            height={200}
+            priority
+            
+          />
+        </div>
         
       </main>
     </div>
