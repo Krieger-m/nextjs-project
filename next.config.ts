09262054fdb86bee04a371453eff6f4644e10a7e
Mat <matze.krieger@googlemail.com>
2025-11-27 14:28:15 +0100

    optimized shareTech for production

--- next.config.ts
+++ next.config.ts
@@ -16,8 +16,13 @@ const nextConfig: NextConfig = {
     ],
     unoptimized: true,
   },
+  experimental: {
+    // Increase server action body size limit to allow image uploads
+    serverActions: {
+      // Accept up to 10 MB request bodies for Server Actions
+      bodySizeLimit: "10mb",
+    },
+  }
 };
 
 export default nextConfig;
-
-
