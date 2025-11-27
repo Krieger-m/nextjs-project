000f1ba1ba30e5949d284b4c8363a89998b58e75
Mat <matze.krieger@googlemail.com>
2025-11-27 14:10:44 +0100

    added form data validation to shareTech server-actions and dynamic error message on the share page using useActionState() from react

--- src/app/technologies/share/page.tsx
+++ src/app/technologies/share/page.tsx
@@ -1,16 +1,20 @@
+'use client';
 import styles from "@/styles/page.module.css";
 import { Metadata } from "next";
 import { ImagePicker } from "@/_components/ImagePicker";
 import { shareTech } from "@/data/actions";
 import { TechFormSubmitButton } from "@/_components/TechFormSubmitButton";
+import { useActionState } from "react";
 
-export const metadata: Metadata = {
-  title: "Full-Stack-Project",
-  description: "Tecnologies Page",
-};
+// export const metadata: Metadata = {
+//   title: "Full-Stack-Project",
+//   description: "Tecnologies Page",
+// };
 
 export default function ShareTechPage() {
   console.log(": executing Technologies/share ...");
+
+  const [state, formAction] = useActionState(shareTech, undefined)
 
 
 
@@ -42,7 +46,7 @@ export default function ShareTechPage() {
             <p>Or any other tech you feel needs sharing!</p>
           </header>
           <main className={styles.main}>
-            <form className={styles.form} action={shareTech}>
+            <form className={styles.form} action={formAction}>
               <div className={styles.row}>
                 <p>
                   <label htmlFor="creator">Creator</label>
@@ -78,6 +82,7 @@ export default function ShareTechPage() {
               <div className={styles.row}>
                 <ImagePicker label={"image picker"} name={"image"} />
               </div>
+              {(state?.message)&&(<h3 style={{color: 'red', textAlign: 'center',margin: '40px auto'}}>{state?.message}</h3>)}
               <p className={styles.actions}>
                 <TechFormSubmitButton/>
               </p>
--- src/data/actions.ts
+++ src/data/actions.ts
@@ -1,24 +1,38 @@
-'use server';
+"use server";
 
 import { redirect } from "next/navigation";
 import { saveTech, TechProps } from "./tech";
 
-export async function shareTech(formData: FormData) {
-    "use server";
+type State = { message: string };
 
-    const tech = {
-      // id: formData.get('id'),
-      // slug: formData.get('slug'),
-      title: formData.get("title"),
-      image: formData.get("image"),
-      summary: formData.get("summary"),
-      instructions: formData.get("instructions"),
-      creator: formData.get("creator"),
-      creator_email: formData.get("creator_email"),
-    };
-    console.log("tech:");
-    console.log(tech);
-    await saveTech(tech as TechProps);
 
-    redirect('/technologies');
+function isInvalidInput(text: FormDataEntryValue | null) {
+  return !text || String(text).trim() === "";
+}
+export async function shareTech(
+  prevState: State | undefined,
+  formData: FormData
+): Promise<State | undefined> {
+  const tech = {
+    title: formData.get("title"),
+    image: formData.get("image"),
+    summary: formData.get("summary"),
+    instructions: formData.get("instructions"),
+    creator: formData.get("creator"),
+    creator_email: formData.get("creator_email"),
+  };
+
+  if (
+    !tech.title ||
+    !tech.summary ||
+    !tech.instructions ||
+    !tech.creator ||
+    !tech.creator_email ||
+    !String(tech.creator_email).includes("@")
+  ) {
+    return { message: "Invalid input!" };
   }
+
+  await saveTech(tech as TechProps);
+  redirect("/technologies");
+}
